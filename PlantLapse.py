f3dc9ce2f4f4f86a824ea51185f1933312ae2b6f
elocremarc <elocremarc@gmail.com>
2019-01-10 15:52:02 -0800

    Combined both python files into one script which allowed the timestamp to change which keeps the files from being overwritten.

--- PlantLapse.py
+++ PlantLapse.py
@@ -15,20 +15,54 @@ from time import sleep
 import datetime
 from sh import gphoto2 as gp 
 import signal, os, subprocess
-from CaptureImage import CaptureImage 
+
 
 growpin = 2
 studiopin = 3
 daytime = 1 
-interval = 25
+interval = 25 #Set the Timelapse Interval
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(growpin, GPIO.OUT)
 GPIO.setup(studiopin, GPIO.OUT)
+ 
 
- #Set the Timelapse Interval
+#This is where you specify the name of the project for filenaming
+picID = "PlantShots"
 
+shot_date = datetime.datetime.now().strftime("%Y-%m-%d")
+shot_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+
+# Remove Residual Capture1 & Capture2 from folder that coud hang the Gphoto2 bash script
+def cleanFolder ():
+	if os.path.exists("capture1.jpg"):
+		os.remove("capture1.jpg")
+	if os.path.exists("capture2.cr2"):
+		os.remove("capture2.cr2")
+
+# Move to working directory, Using gphoto2, Capture Image, Download, Rename jpg file to Capture 1 & the raw file to Capture 2	
+def gphotoCapture ():
+	os.system("cd /home/pi/HDD && gphoto2 --capture-image-and-download --filename capture%n.%C")
+
+# Rename Files with Timestamp and Project name
+def renameFiles(ID):
+    for filename in os.listdir("."):
+        if len(filename) < 13:
+            if filename.endswith(".jpg"):
+                os.rename(filename, (shot_time + ID + ".JPG"))
+                print("Renamed the JPG")
+            if filename.endswith(".cr2"):
+                os.rename(filename, (shot_time + ID + ".CR2"))
+                print("Renamed the cr2")
+
+# Combine functions in order of operation to take a picture onto the pi  				
+def CaptureImage ():
+	cleanFolder()	
+	gphotoCapture()	
+	renameFiles(picID)
 
 #This function determines if the sun is up based on the range in the if statement
 def suncheck():
@@ -65,7 +99,9 @@ def growLight():
 		
 while True:
 
-
+	shot_date = datetime.datetime.now().strftime("%Y-%m-%d")
+	shot_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	
 	# Setup GPIO set growpin and studiopin as outputs
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(growpin, GPIO.OUT)
